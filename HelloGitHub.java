f0d2f4224cfe46a1e57ab36da38a7d594c67326b
vibhatha <vibhatha@gmail.com>
2015-04-30 08:39:09 -0700

    First Commit

---
+++ HelloGitHub.java
@@ -0,0 +1,13 @@
+class HelloGitHub{
+
+	public static void main(String [] args){
+
+		System.out.println("Hello GitHub!!!");
+		//git add filename.extension ==> adding a file to git hub
+		//git status => checking the status
+		//git commit -m "your message here"
+		//git push => sync it with github.com
+		//git add -a =? add all to github
+
+	}
+}
